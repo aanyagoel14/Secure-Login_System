e965c66fe5aa5ddc6edb021ca54060e2724347f6
Aanya Goel <131501514+aanyagoel14@users.noreply.github.com>
2025-05-25 18:13:38 +0000

    Connecting to the database

--- database.php
+++ database.php
@@ -2,7 +2,7 @@
 
 require_once('config.php');
 
-function get_db_connection()
+function db_connection()
   try{  
     $db = new PDO('mysql:host=' . DB_HOST, DB_USER, DB_PASSWORD, ';dbname=' . DB_DATABASE, ';port=' . DB_PORT);
     return $db;
