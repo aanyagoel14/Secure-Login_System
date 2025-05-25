ca58e9a1de89a2ef15d70c912e4cc9bdf4a43722
Aanya Goel <131501514+aanyagoel14@users.noreply.github.com>
2025-05-25 18:12:41 +0000

    Defining database credentials

--- config.php
+++ config.php
@@ -1,9 +1,7 @@
 <?php
-
 define`('DB_HOST',    'yr--a.h.filess.io');
 define('DB_USER',     'COSC4806_suitoneher');
 define('DB_PASSWORD', $_ENV['DB_PASSWORD']);
 define('DB_DATABASE', 'COSC4806_suitoneher');
 define('DB_PORT',     '3305')
-
 ?>  
